ba9db57a9bfbe657ee10aadce50a83463530fb71
Gor Hakobyan <gorhakobyan5577@gmail.com>
2019-05-30 16:57:17 +0400

    Main

--- src/com/aca/Main.java
+++ src/com/aca/Main.java
@@ -1,10 +1,5 @@
 package com.aca;
 
-import com.aca.maximum.MaxNumber;
-import com.aca.textReverse.TextReverse;
-
-import java.util.ArrayList;
-
 public class Main {
 
     public final static int COUNT = 10_000_000;
@@ -14,15 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-/*
-        MaxNumber maxNumber = new MaxNumber();
-        maxNumber.findMaxValueWithThreads();
-*/
-/*
 
-        TextReverse textReverse = new TextReverse();
-        textReverse.reverseString("I am programmer");
-*/
 
     }
 
